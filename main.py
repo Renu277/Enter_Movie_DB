13029c1979433c0e1169ae0fc1f21fd2ad6f5306
Renu277 <renu.kanjarla@gmail.com>
2023-05-19 12:15:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+moviesList = []
+
+@app.route('/', methods=['GET', 'POST'])
+def movies_manager():
+    if request.method == 'POST':
+        choice = request.form['choice']
+        if choice == '1':
+            return redirect(url_for('add_movie'))
+        elif choice == '2':           
+           return redirect(url_for('all_movies'))
+        elif choice == '3':           
+           return print('-----')
+    return render_template('index.html', moviesList=moviesList)
+
+
+@app.route('/add_movie', methods=['GET', 'POST'])
+def add_movie():
+    if request.method == 'POST':
+        nName = request.form['name']
+        nGenre = request.form['genre']
+        nRating = request.form['ratings']
+        moviesList.append([nName, nGenre, nRating])
+        return redirect(url_for('movies_manager'))
+    return render_template('add_movie.html')
+@app.route('/all_movies')
+def all_movies():
+    return render_template('all_movies.html', moviesList=moviesList)
+
+if __name__ == '__main__':
+    app.run(debug=True)
